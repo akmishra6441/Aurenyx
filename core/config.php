f0089a2e09132b34417cc13f15baf91f593ac0b7
Abhishekh Mishra <mishraabhishekh6441@gmail.com>
2025-09-18 20:59:20 +0530

    Create config.php

---
+++ core/config.php
@@ -0,0 +1,9 @@
+<?php
+// Database Credentials
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root'); // Your DB username
+define('DB_PASS', ''); // Your DB password
+define('DB_NAME', 'social_network_db');
+
+// Start the session
+session_start();
