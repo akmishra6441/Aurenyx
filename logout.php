88a0b2c4b6933670dbbf7d671e54644ccfbbdab7
Abhishekh Mishra <mishraabhishekh6441@gmail.com>
2025-09-24 18:12:49 +0530

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,12 +1,9 @@
 <?php
 require_once 'core/config.php';
 
-// Unset all session variables
 $_SESSION = array();
 
-// Destroy the session
 session_destroy();
 
-// Redirect to login page
 header('location: login.php');
 exit();
