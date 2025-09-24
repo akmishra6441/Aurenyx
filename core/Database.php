a82d58c93ab19451e54d7a7edaeb2350df78ff33
Abhishekh Mishra <mishraabhishekh6441@gmail.com>
2025-09-24 18:24:53 +0530

    Update Database.php

--- core/Database.php
+++ core/Database.php
@@ -6,19 +6,17 @@ class Database {
     private $pass = DB_PASS;
     private $dbname = DB_NAME;
 
-    private $dbh; // Database handler
-    private $stmt; // Statement
+    private $dbh; 
+    private $stmt; 
     private $error;
 
     public function __construct() {
-        // Set DSN (Data Source Name)
         $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
         $options = array(
             PDO::ATTR_PERSISTENT => true,
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         );
 
-        // Create PDO instance
         try {
             $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
         } catch (PDOException $e) {
@@ -27,12 +25,10 @@ class Database {
         }
     }
 
-    // Prepare statement with query
     public function query($sql) {
         $this->stmt = $this->dbh->prepare($sql);
     }
 
-    // Bind values
     public function bind($param, $value, $type = null) {
         if (is_null($type)) {
             switch (true) {
@@ -52,24 +48,20 @@ class Database {
         $this->stmt->bindValue($param, $value, $type);
     }
 
-    // Execute the prepared statement
     public function execute() {
         return $this->stmt->execute();
     }
     
-    // Get result set as array of objects
     public function resultSet() {
         $this->execute();
         return $this->stmt->fetchAll(PDO::FETCH_OBJ);
     }
 
-    // Get single record as object
     public function single() {
         $this->execute();
         return $this->stmt->fetch(PDO::FETCH_OBJ);
     }
 
-    // Get row count
     public function rowCount() {
         return $this->stmt->rowCount();
     }
