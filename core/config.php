a878c4d396b816598897eb4664601a048fa7c434
Abhishekh Mishra <mishraabhishekh6441@gmail.com>
2025-09-24 18:25:10 +0530

    Update config.php

--- core/config.php
+++ core/config.php
@@ -1,9 +1,7 @@
 <?php
-// Database Credentials
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root'); // Your DB username
-define('DB_PASS', ''); // Your DB password
+define('DB_USER', 'root'); 
+define('DB_PASS', ''); 
 define('DB_NAME', 'social_network_db');
 
-// Start the session
 session_start();
